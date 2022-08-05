e3733c603fc6f641fa0a0c37075b48ebd7df84fc
Areola John <areolajohnbayo@outlook.com>
2022-08-05 15:56:18 +0100

    Add project files.

---
+++ NUrtw/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace FunMiLowoMiJo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicles vehicleCode = new Vehicles();
+            int totalAmount = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int vehicleFee = vehicleCode.Vehicle_Type();
+                int trip = vehicleCode.Trip();
+                double membershipRate = vehicleCode.MembershipRate();
+
+                double amount = vehicleFee * trip * membershipRate;
+                int amountInt = Convert.ToInt32(amount);
+                totalAmount = totalAmount + amountInt;
+                Console.WriteLine("FUN MI NI " + amountInt + ". fun mi lo wo mi joor \n");
+            }
+            Console.WriteLine("The total amount is " + totalAmount);
+
+        }
+    }
+}
---
+++ NUrtw/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunMiLowoMiJo
+{
+    internal class Vehicles
+    {
+        public int Vehicle_Type()
+        {
+            Console.WriteLine("if Vehicle_Type is Okada enter 1");
+            Console.WriteLine("if Vehicle_Type is Keke enter 2");
+            Console.WriteLine("if Vehicle_Type is Korope enter 3");
+            Console.WriteLine("if Vehicle_Type is Molue enter 4");
+            string vehicleType = Console.ReadLine();
+            switch (vehicleType)
+            {
+                case "1":
+                    return 10;
+                case "2":
+                    return 20;
+                case "3":
+                    return 30;
+                case "4":
+                    return 40;
+                default:
+                    return 0;
+            }
+        }
+
+        public int Trip()
+        {
+            Console.WriteLine("How many trips have you completed today?");
+            int trip = Convert.ToInt32(Console.ReadLine());
+            return trip;
+        }
+
+        public double MembershipRate()
+        {
+            Console.WriteLine("Are you a member? (yes/no)");
+            string membership = Console.ReadLine().ToLower();
+            if (membership == "yes")
+            {
+                double membershipRate = 0.7d;
+                return membershipRate;
+            }
+            else if (membership == "no")
+            {
+                double membershipRate = 1d;
+                return membershipRate;
+            }
+            else
+            {
+                return 0;
+            }
+            //if True membership
+            //int membershipRate = Console.ReadLine(); 
+
+        }
+
+    }
+}
